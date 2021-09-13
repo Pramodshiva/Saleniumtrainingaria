1f3c3c3c0fbda12634f702db47d38f0219ebc97a
pramodshivashankara-agi <88143310+pramodshivashankara-agi@users.noreply.github.com>
2021-09-13 13:46:25 +0530

    c

--- selenium/src/main/java/DAY5/Addingdataprovider.java
+++ selenium/src/main/java/DAY5/Addingdataprovider.java
@@ -9,7 +9,7 @@ public Object[][]providedata(){
 	return new Object[][] {{2},{3}};
 }
 @Test(dataProvider = "dataprovider")
-public void sum(int v1){
+public void sum(String v1){
 	System.out.println(v1);
 }
 }
